dc3c8dba251e811e721f944ad6dca4e04fdb4901
Mehak Noor Singh <mehaknoor2k2@gmail.com>
2023-01-25 00:38:21 +0530

    feat: complete api for todos app

--- BackEnd/httpServer.js
+++ BackEnd/httpServer.js
@@ -1,10 +1,141 @@
 const http = require('http');
-const PORT = 8000;
 
-const server = http.createServer((req,res)=>{
-    res.writeHead(200,{'Content-Type':'text/html'});
-    res.write('<h1>Hello World</h1>');
-    res.end();
+const PORT = 8000;
+const data = [];
+
+
+const server = http.createServer((req, res) => {
+
+    // run GET and POST on /todos
+    if (req.url === '/todos') {
+        let method = req.method.toUpperCase();
+
+        switch (method) {
+        case 'GET': {
+            const sendData = JSON.stringify(data);
+            res.writeHead(200, { 'Content-Type': 'JSON' });
+            res.end(sendData);
+            break;
+        }
+
+        case 'POST': {
+            let dataSent = '';
+
+            req.on('data', (data) => {
+                dataSent += data;
+            });
+
+            req.on('end', () => {
+
+                dataSent = JSON.parse(dataSent);
+
+                dataSent['isComplete'] = false;
+                if (data.length == 0)
+                    dataSent['id'] = 0;
+                else
+                    dataSent['id'] = Number(data[data.length - 1]['id'] + 1);
+                data.push(dataSent);
+
+                res.writeHead(201, { 'Content-Type': 'JSON' });
+                res.end(JSON.stringify(data));
+
+            });
+            break;
+        }
+
+        // delete all tasks which have isCompleted true
+        case 'DELETE': {
+            data.forEach((ele, index) => {
+                if (ele['isComplete'] === true)
+                    data.splice(index);
+            });
+            res.writeHead(200, { 'Contet-Type': 'text' });
+            res.end('All Completed tasks have been deleted successfully.');
+            break;
+        }
+
+
+        default:
+            break;
+        }
+    }
+
+    else if (req.url.match('/todos/') != null) {
+
+        let idRequested = Number(req.url.substring(7));
+
+        // 422 Unprocessable Entity
+        let idFoundFlag = false, idFound = 0;
+        data.forEach((ele,index)=>{
+            if(ele['id'] == idRequested)
+                idFoundFlag = true,idFound = index;
+        });
+
+        if (idFoundFlag == false) {
+
+            if (req.method.toUpperCase() == 'POST') {
+                res.writeHead(422, { 'Content-Type': 'text/html' });
+                res.end('You cannot post in this endpoint. Use /todos for POST request');
+            }
+            else {
+                res.writeHead(422, { 'Content-Type': 'text/html' });
+                res.end('The id send as parameter is inavalid or does not exist.');
+            }
+
+        }
+        else
+            switch (req.method.toUpperCase()) {
+
+            case 'GET': {
+                res.writeHead(200, { 'Content-Type': 'JSON' });
+                res.end(JSON.stringify(data[idFound]));
+                break;
+            }
+
+            case 'POST': {
+                res.writeHead(403, { 'Content-Type': 'text/html' });
+                res.end('You cannot post in this endpoint. Use /todos for POST request');
+                break;
+            }
+
+            case 'DELETE': {
+                delete data.splice(idFound,1);
+                res.writeHead(200, { 'Content-Type': 'text/html' });
+                res.end('The data corresponding to id has been deleted successfully.');
+                break;
+            }
+
+            case 'PATCH': {
+                let dataSent = '';
+
+                req.on('data', (data) => {
+                    dataSent += data;
+                });
+
+                req.on('end', () => {
+
+                    dataSent = JSON.parse(dataSent);
+                    for (let key in dataSent)
+                        data[idFound][key] = dataSent[key];
+
+                    res.writeHead(200, { 'Content-Type': 'JSON' });
+                    res.end(JSON.stringify(data));
+
+                });
+
+                break;
+            }
+
+            default:
+                break;
+            }
+
+    }
+    else {
+        res.writeHead(404, { 'Content-Type': 'text/html' });
+        res.end('<h1>404 Page Not Found</h1>');
+    }
+
 });
 
-server.listen(PORT,()=> console.log(`Server is running on http://localhost:${PORT}`));
+server.listen(PORT, () => console.log(`Server is running on http://localhost:${PORT}`));
