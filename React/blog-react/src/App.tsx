62d5a65849932ba01bbbdd5bf33d89b17385979b
Mehak Noor Singh <mehaknoor2k2@gmail.com>
2023-02-20 11:58:09 +0530

    feat: complete blog react

---
+++ React/blog-react/src/App.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "./Components/Header/Header";
+import CardContainer from "./Components/CardContainer/CardContainer";
+import "./App.css";
+import Footer from "./Components/Footer/Footer";
+
+function App() {
+    return (
+        <div>
+            <Header />
+            <CardContainer />
+            <Footer />
+        </div>
+    );
+}
+
+export default App;
